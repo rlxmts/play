b560f3caea9818dc15f78665fd74f7fbbb203271
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-05-17 11:28:38 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const listaDeMusicas = document.querySelectorAll('.musica');
+const playerBt = document.querySelector('.bt_player');
+let audio = null;
+
+listaDeMusicas.forEach( musica => {
+    
+    musica.onclick = ()=> {
+
+        if(audio != null){
+            audio.pause()
+        }
+        let musicaEscolhida = musica.classList[1];        
+        audio = new Audio(`./audio/${musicaEscolhida}.mp3`);    
+        audio.play();
+    };
+    
+})
+
+
+playerBt.addEventListener( 'click', ()=> {
+
+    let primeiraFaixa = listaDeMusicas[0];
+    let nomePrimeiraFaixa = primeiraFaixa.classList[1];
+    tocarMusica(nomePrimeiraFaixa);
+
+})
