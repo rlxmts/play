1dcc9b5b133643e1c9c07748db7dd0aac8906d78
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-05-17 12:06:18 -0300

    :dizzy: Add cor ao titulo da musica

--- index.js
+++ index.js
@@ -2,25 +2,37 @@ const listaDeMusicas = document.querySelectorAll('.musica');
 const playerBt = document.querySelector('.bt_player');
 let audio = null;
 
+
 listaDeMusicas.forEach( musica => {
     
-    musica.onclick = ()=> {
-
+    musica.onclick = ()=> {                
+        const musicaAtual = musica.querySelector('.musica_titulo');
         if(audio != null){
-            audio.pause()
-        }
+            audio.pause();
+        }        
         let musicaEscolhida = musica.classList[1];        
         audio = new Audio(`./audio/${musicaEscolhida}.mp3`);    
         audio.play();
+        addERemoveClasse(musicaAtual);  
     };
-    
 })
 
-playerBt.addEventListener( 'click', ()=> {
+function addERemoveClasse(musicaAtual){
+    const textoMusica = document.querySelectorAll('.musica_titulo');
+    textoMusica.forEach( musica => {
+        musica.classList.remove('tocando');
+    });
+    
+    musicaAtual.classList.add('tocando');
+}
 
+playerBt.addEventListener( 'click', ()=> {
     let primeiraFaixa = listaDeMusicas[0];
     let nomePrimeiraFaixa = primeiraFaixa.classList[1];
-
+    if(audio != null){
+        audio.pause();
+    }
     audio = new Audio(`./audio/${nomePrimeiraFaixa}.mp3`);
     audio.play();
 })
+
