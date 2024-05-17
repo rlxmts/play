0c63af7a561dec050b43bd51993184c61a605b83
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-05-17 12:53:24 -0300

    :recycle: Ultimos ajustes e melhorias no código

--- index.js
+++ index.js
@@ -9,40 +9,55 @@ botaoParar.addEventListener( 'click' , (e)=> {
     e.preventDefault();
     controles.classList.remove('controles-ativo');
     audio.pause();
+    removeClasses();
 })
-
 
 listaDeMusicas.forEach( musica => {
     
-    musica.onclick = ()=> {                
+    musica.onclick = ()=> {      
         const musicaAtual = musica.querySelector('.musica_titulo');
+
         if(audio != null){
             audio.pause();
         }        
-        let musicaEscolhida = musica.classList[1];        
+
+        let musicaEscolhida = musica.classList[1];  
+
         audio = new Audio(`./audio/${musicaEscolhida}.mp3`);    
         audio.play();
+
+        removeClasses();  
+        musicaAtual.classList.add('tocando');
         controles.classList.add('controles-ativo');
-        addERemoveClasse(musicaAtual);  
     };
 })
 
-function addERemoveClasse(musicaAtual){
+function removeClasses(){
+    
     const textoMusica = document.querySelectorAll('.musica_titulo');
+    
     textoMusica.forEach( musica => {
         musica.classList.remove('tocando');
     });
     
-    musicaAtual.classList.add('tocando');
 }
 
 playerBt.addEventListener( 'click', ()=> {
-    let primeiraFaixa = listaDeMusicas[0];
-    let nomePrimeiraFaixa = primeiraFaixa.classList[1];
+    
+    removeClasses();
+    
+    const primeiraFaixa = listaDeMusicas[0];
+    const nomePrimeiraFaixa = primeiraFaixa.classList[1];
+    const musica = primeiraFaixa.querySelector('.musica_titulo');
+    
     if(audio != null){
         audio.pause();
     }
+    
     audio = new Audio(`./audio/${nomePrimeiraFaixa}.mp3`);
     audio.play();
+    
+    musica.classList.add('tocando');
+    controles.classList.add('controles-ativo');
 })
 
